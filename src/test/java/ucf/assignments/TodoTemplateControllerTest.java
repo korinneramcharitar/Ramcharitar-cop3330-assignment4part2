35398a4a52508257b2b1f244dfcc8741a6d5ffa0
korinneramcharitar <77815286+korinneramcharitar@users.noreply.github.com>
2021-07-11 22:55:34 -0400

    chnaged directory

--- src/test/java/ucf/assignments/TodoTemplateControllerTest.java
+++ src/test/java/ucf/assignments/TodoTemplateControllerTest.java
@@ -1,59 +1,132 @@
 package ucf.assignments;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TodoTemplateControllerTest {
 
-    //TableView allows for certain features already included such as sort, so no test method will be provided for it
-    //probably need to set certain cell values, not sure yet
+    @FXML
+    public TableColumn<ucf.assignments.Tasks, LocalDate> DueDateColumn;
+    @FXML
+    public TableView<ucf.assignments.Tasks> TodoListTable;
+    @FXML
+    public TextField TodoListTitle;
 
-    //test to see if user can add tasks
-    @Test
-    void addTaskButtonClicked() {
-        //recieve textfield input text
-        //compare TableView info to input text
-        //assert input is now displayed in TableView
+
+    public class Task {
+        private SimpleStringProperty taskName;
+        private SimpleStringProperty taskDescription;
+
+
+        public Task(String taskName, String taskDescription) {
+            this.taskName = new SimpleStringProperty(taskName);
+            this.taskDescription = new SimpleStringProperty(taskDescription);
+
+        }
+
+
+
+        public String getTaskDescription() {
+            return taskDescription.get();
+        }
+
+        public SimpleStringProperty taskDescriptionProperty() {
+            return taskDescription;
+        }
+
+        public void setTaskDescription(String taskDescription) {
+            this.taskDescription.set(taskDescription);
+        }
+
+        public String getTaskName() {
+            return taskName.get();
+        }
+
+        public SimpleStringProperty taskNameProperty() {
+            return taskName;
+        }
+
+        public void setTaskName(String taskName) {
+            this.taskName.set(taskName);
+        }
+
     }
-    //test to see if user can delete tasks
-    @Test
-    void deleteTaskButtonClicked() {
-     //recieve textfield input text
-        //compare text to TableView info
-        //assert the input is no longer available on TableView
 
-    }
-    //test to check if complete tasks will show
-    @Test
-    void completeTasksButtonClicked() {
-        //check if after event tableview colums appearing have checkmarks in CheckBox column
+    public class Tasks {
+        private SimpleStringProperty taskTitle;
+
+        public Tasks(String taskTitle) {
+            this.taskTitle = new SimpleStringProperty(taskTitle);
+
+        }
 
 
-    }
-//test to check if incomplete tasks will show
-    @Test
-    void incompleteTasksButtonClicked() {
-        //check if after event tableview colums appearing have no checkmarks in CheckBox column
-    }
-    //test to check if tasks are editable
-    @Test
-    void changeTask() {
-        //check tableview is editable in Task Column
+        public String getTaskTitle() {
+            return taskTitle.get();
+        }
 
+        public SimpleStringProperty taskTitleProperty() {
+            return taskTitle;
+        }
 
-    }
-    //test to check if descriptions are editable
-    @Test
-    void changeDesrip() {
-        //check tableview is editable in Description Column
+        public void setTaskTitle(String taskTitle) {
+            this.taskTitle.set(taskTitle);
+        }
     }
 
-//test to check if dates are editable
     @Test
-    void changeDate() {
-        //check tableview is editable in Due Date Column
-        //these changes are initiated by user double clicking cells
-        //maybe UserClick() neccessary
+    public void showInformation() {
+
+        Tasks task = new Tasks("WatchList");
+    }
+
+    @Test
+    void saveListButtonClicked() {
+
+        try {
+            Task task = new Task("Buy groceries", "Buy lemons");
+
+            FileWriter myWriter = new FileWriter("/Users/korinneramcharitar/Desktop/Output.txt");
+
+
+                String formatted = String.format("%s, %s",  task.getTaskName(), task.getTaskDescription());
+                myWriter.write(formatted);
+
+
+                System.out.println(formatted);
+
+
+            myWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+        @Test
+    void uploadListButtonClicked() {
+        String path = "/Users/korinneramcharitar/Desktop/SampleTodoList";
+        String line = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                System.out.println(line);
+                LocalDate localDate = LocalDate.parse(values[0]);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
