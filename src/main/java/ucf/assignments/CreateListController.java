6c3a6f3dd2937522876ddc6765244778a56ebbc1
korinneramcharitar <77815286+korinneramcharitar@users.noreply.github.com>
2021-07-11 20:34:12 -0400

    chnaged directory

--- src/main/java/ucf/assignments/CreateListController.java
+++ src/main/java/ucf/assignments/CreateListController.java
@@ -1,15 +1,20 @@
 package ucf.assignments;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class CreateListController {
+    @FXML
+    public TextField EnterListTextField;
+
     public void HomeButtonClicked(ActionEvent actionEvent) {
         try {
             Parent HomeListParent = FXMLLoader.load(getClass().getResource("Welcome.fxml"));
@@ -24,9 +29,23 @@ public class CreateListController {
 
     }
 
-    public void CreateListEnterButtonClicked(ActionEvent actionEvent) {
+    public void CreateListEnterButtonClicked(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("TodoTemplate.fxml"));
+        Parent root = loader.load();
+
+
+        TodoTemplateController TodoListScene = loader.getController();
+        TodoListScene.showInformation(EnterListTextField.getText());
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setTitle("TodoList Data");
+        stage.show();
+
+
         //takes input from textfield and passes text to next scene where it will be displayed
         //next scene will be where user can add tasks to list
         //stores data in CreateListModel class for list name association to be used later
     }
+
+
 }
---
+++ src/main/java/ucf/assignments/Tasks.java
@@ -0,0 +1,67 @@
+package ucf.assignments;
+
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import java.time.LocalDate;
+
+
+public class Tasks {
+    private SimpleStringProperty taskName;
+    private SimpleStringProperty taskDescription;
+    private DatePicker taskDueDate;
+    private CheckBox Checkoff;
+
+
+    public Tasks(String taskName, String taskDescription, LocalDate taskDueDate) {
+        this.taskName = new SimpleStringProperty(taskName);
+        this.taskDescription = new SimpleStringProperty(taskDescription);
+        this.taskDueDate = new DatePicker(taskDueDate);
+        this.Checkoff = new CheckBox();
+
+
+    }
+
+
+
+    public CheckBox getCheckoff() {
+        return Checkoff;
+    }
+
+    public void setCheckoff(CheckBox checkoff) {
+        Checkoff = checkoff;
+    }
+
+    public DatePicker getTaskDueDate() {
+        return taskDueDate;
+    }
+
+    public void setTaskDueDate(DatePicker taskDueDate) {
+        this.taskDueDate = taskDueDate;
+    }
+
+    public SimpleStringProperty taskNameProperty() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName.set(taskName);
+    }
+
+    public String getTaskDescription() {
+        return taskDescription.get();
+    }
+
+    public SimpleStringProperty taskDescriptionProperty() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription.set(taskDescription);
+    }
+
+    public Object getTaskName() {
+        return taskName.get();
+    }
+}
